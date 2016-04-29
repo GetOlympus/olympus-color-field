8b1fd031c5cae1b98136670b1dffe02cd4212835
crewstyle <ach@takeatea.com>
2016-04-29 10:33:52 +0200

    first commit

---
+++ src/Color/Color.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace GetOlympus\Field;
+
+use GetOlympus\Hera\Controllers\Field;
+use GetOlympus\Hera\Controllers\Translate;
+
+/**
+ * Builds Color field.
+ *
+ * @package Field
+ * @subpackage Color
+ * @author Achraf Chouk <achrafchouk@gmail.com>
+ * @since 0.0.1
+ *
+ * @see https://olympus.readme.io/v1.0/docs/color-field
+ *
+ */
+
+class Color extends Field
+{
+    /**
+     * @var string
+     */
+    protected $faIcon = 'fa-tint';
+
+    /**
+     * @var string
+     */
+    protected $template = 'color.html.twig';
+
+    /**
+     * Prepare HTML component.
+     *
+     * @param array $content
+     * @param array $details
+     *
+     * @since 0.0.1
+     */
+    protected function getVars($content, $details = [])
+    {
+        // Build defaults
+        $defaults = [
+            'id' => '',
+            'title' => Translate::t('color.title'),
+            'default' => '',
+            'description' => '',
+
+            // details
+            'post' => 0,
+            'prefix' => '',
+            'template' => 'pages',
+        ];
+
+        // Build defaults data
+        $vars = array_merge($defaults, $content);
+
+        // Retrieve field value
+        $vars['val'] = $this->getValue($details, $vars['default'], $content['id'], true);
+
+        // Update vars
+        $this->getField()->setVars($vars);
+    }
+}
